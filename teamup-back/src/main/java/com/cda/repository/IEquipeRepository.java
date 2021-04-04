2b3d7a3fc0381b05dfc1a683331230024a1177cd
samir <samirardjouni>
2021-04-04 21:57:49 +0200

    creation des Interfaces Repository

---
+++ teamup-back/src/main/java/com/cda/repository/IEquipeRepository.java
@@ -0,0 +1,9 @@
+package com.cda.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cda.model.Equipe;
+
+public interface IEquipeRepository extends JpaRepository<Equipe, Integer> {
+
+}
---
+++ teamup-back/src/main/java/com/cda/repository/IStatistiqueRepository.java
@@ -0,0 +1,9 @@
+package com.cda.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cda.model.Statistique;
+
+public interface IStatistiqueRepository  extends JpaRepository<Statistique, Integer>{
+
+}
