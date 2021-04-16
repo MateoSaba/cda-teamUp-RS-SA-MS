953b99ccd63dca483230222c5e9cf791f71f4a16
MateoSaba <mtosaba@gmail.com>
2021-04-16 09:32:31 +0200

    Component login.jsx front terminé + debut de la navbar.jsx

---
+++ teamup-front/src/app/components/header-footer/LoginForm.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import '../../assets/css/LoginFormCss.css'
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <div className="wrapper fadeInDown">
+            <div id="formContent">
+         
+            <div class="fadeIn first">
+      <img  src="https://img.icons8.com/cotton/64/000000/football-ball.png" alt=""/>
+      <img  src="https://img.icons8.com/cotton/64/000000/football-ball.png" alt=""/>
+      <img  src="https://img.icons8.com/cotton/64/000000/football-ball.png" alt=""/>
+      <img  src="https://img.icons8.com/cotton/64/000000/football-ball.png" alt=""/>
+    </div>
+
+              <form>
+              <input type="text" id="login" class="fadeIn second" name="login" placeholder="login"/>
+                <input type="text" id="password" class="fadeIn third" name="login" placeholder="password"/>
+                  <input type="submit" class="fadeIn fourth" value="Log In"/>
+              </form>
+
+          
+            </div>
+          </div>
+        );
+    }
+}
+
+export default LoginForm;
