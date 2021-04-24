76d372f36edf53a39bd335d5fb815c957546044a
MateoSaba <mtosaba@gmail.com>
2021-04-24 12:32:27 +0200

    Statistique.jsx

--- teamup-front/src/app/views/Statistique.jsx
+++ teamup-front/src/app/views/Statistique.jsx
@@ -1,13 +1,70 @@
 import React, { Component } from 'react';
+import '../assets/css/Statistique.css'
+import '../assets/css/pagecontent.css'
 
 class Statistique extends Component {
-    render() {
-        return (
-            <div>
-                
+  render() {
+    return (
+
+      <div>
+
+
+        <div className="row page-content">
+
+          <div className="wrapper col-md-4">
+            <div id="formContent" className="hauteur">
+              <h1  className="mtop">Match joués</h1> <br />
+              <h1>10</h1>
             </div>
-        );
-    }
+          </div>
+
+          <div className="wrapper col-md-4">
+            <h1 className="titre-profil">Mes stats</h1>
+            <div id="formContent" className="hauteur">
+              <h1  className="mtop">Victoire</h1> <br />
+              <h1>10</h1>
+            </div>
+          </div>
+
+
+
+          <div className="wrapper col-md-4">
+            <div id="formContent" className="hauteur">
+              <h1  className="mtop">Défaite</h1> <br />
+              <h1>10</h1>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="row page-content">
+
+          <div className="wrapper col-md-4">
+            <div id="formContent" className="hauteur">
+              <h1 className="mtop">Buts</h1> <br />
+              <h1>10</h1>
+            </div>
+          </div>
+
+          <div className="wrapper col-md-4">
+            <div id="formContent" className="hauteur">
+              <h1  className="mtop">Meilleur joueur</h1> <br />
+              <h1>10</h1>
+            </div>
+          </div>
+
+
+          <div className="wrapper col-md-4">
+            <div id="formContent" className="hauteur">
+              <h1  className="mtop">Pire joueur</h1> <br />
+              <h1>10</h1>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Statistique;
